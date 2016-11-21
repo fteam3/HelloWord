ba4d90afce200746c98743e91fd46126e426c55d
fteam3 <475201948@qq.com>
2016-11-21 13:38:47 +0800

    commit

---
+++ src/fide/project/First.java
@@ -0,0 +1,11 @@
+package fide.project;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+
+		System.out.println("hello, this is the first test!");
+	}
+
+}
